a73daeea357aa00962ab7ac14f12067717378bb0
JazzSlice <108577355+JazzSlice@users.noreply.github.com>
2024-01-14 19:53:00 +0300

    Update vigenere-en.js
    Реализован метод шифрования Вижинера

---
+++ vigenere-en.js
@@ -0,0 +1,32 @@
+// Имплементация шифра Вижинера на javascript
+// TODO:
+// - реализовать функцию разсшифрования с известным ключем
+// - дополнить словарь символьными значениями
+
+var key = "оладушек";
+var msg = "ПростотЕкст"
+
+var vigenereEncryption = function (message, keyPhrase) {
+    var alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+
+    // Определяем структуру алфавита {index: character} на основе массива с алфавитом
+    var alphabetMap = {};
+    for (var j = 0; j < (alphabet.length - 1); j++) {
+        alphabetMap[alphabet[j]] = j;
+    }
+
+    // заполняем зашифрованное сообщением на основе структуры
+    var encodedMsg = "";
+    for (var k = 0; k < message.length; k++) {
+        /*
+        индекс символа после смещения = индекс символа исходного
+        сообщения + индекс сивола ключа
+        */
+        var charIndex = (alphabetMap[message[k]] + 
+            alphabetMap[keyPhrase[k % (keyPhrase.length)]]);
+        encodedMsg += alphabet[charIndex];
+    }
+    return encodedMsg
+}
+
+console.log(vigenereEncryption(msg, key));
