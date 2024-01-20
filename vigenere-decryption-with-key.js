4285f437dff70ab3b0dbe675adca3e44c366920f
JazzSlice <108577355+JazzSlice@users.noreply.github.com>
2024-01-20 17:51:51 +0300

    Update vigenere-decryption-with-key.js

--- vigenere-decryption-with-key.js
+++ vigenere-decryption-with-key.js
@@ -1,24 +1,27 @@
-var encMessage = "ЮьохЁЖчПщэт";
+var encMessage = "омвжч";
 var key = "оладушек";
 
 var vigenereDecryption = function (message, keyPhrase) {
     var alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
 
-    // Определяем структуру алфавита {character: index} на основе массива с алфавитом
+    // Определяем структуру алфавита {index: character} на основе массива с алфавитом
     var alphabetMap = {};
     for (var j = 0; j < (alphabet.length - 1); j++) {
         alphabetMap[alphabet[j]] = j;
     }
 
-    // заполняем расшифрованное сообщением на основе структуры
+    // заполняем зашифрованное сообщением на основе структуры
     var decodedMsg = "";
     for (var k = 0; k < message.length; k++) {
         /*
         индекс символа после смещения = индекс символа исходного
-        сообщения - индекс сивола ключа
+        сообщения + индекс сиmвола ключа
         */
         var charIndex = (alphabetMap[message[k]] - alphabetMap[keyPhrase[k % (keyPhrase.length)]]);
-        decodedMsg += alphabet[Math.abs(charIndex)];
+        if (charIndex < 0) {
+            charIndex = alphabet.length - Math.abs(charIndex);
+        }
+        decodedMsg += alphabet[charIndex];
     }
     return decodedMsg;
 
